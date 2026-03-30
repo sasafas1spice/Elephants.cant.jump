90688c1caee3cae18cadca0eaedb732f41760a7a
Scythe Abdel Fattah <sarah.abdel@ue-germany.de>
2026-03-30 22:01:52 +0200

    points :D

--- Assets/Scrips/PeopleMovement.cs
+++ Assets/Scrips/PeopleMovement.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class PeopleMovement : MonoBehaviour
@@ -8,15 +9,21 @@ public class PeopleMovement : MonoBehaviour
     public GameObject pointB;
     public Sprite boomSprite;
     public Sprite grave;
+
+    public TextMeshProUGUI points;
     public float speed = 2.0f;
     public bool canMove;
+    public bool canKill;
     private Vector3 targetPosition;
 
     void Start()
     {
         canMove = true;
+        canKill = true;
         // moving towards point A
         targetPosition = pointA.transform.position;        
+        GameObject[] pointsCounters = GameObject.FindGameObjectsWithTag("Points");
+        points = pointsCounters[0].GetComponent<TextMeshProUGUI>();
     }
 
     void Update()
@@ -46,11 +53,30 @@ public class PeopleMovement : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) // For 3D
     {
         // Check the tag of the other collider that entered the trigger zone
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && canKill == true)
         {
             canMove = false;
+            canKill = false;
 
             Destroy(other.gameObject);
+            
+            int oldPoints = int.Parse(points.text);
+
+            if (isPatron())
+            {
+                oldPoints -= 25;
+
+                if (oldPoints < 0)
+                {
+                    // lose
+                }
+
+                points.SetText(oldPoints.ToString());
+            } else
+            {
+                oldPoints += 5;
+                points.SetText(oldPoints.ToString());
+            }
 
             this.gameObject.GetComponent<SpriteRenderer>().sprite = boomSprite;
 
@@ -68,5 +94,16 @@ public class PeopleMovement : MonoBehaviour
         yield return new WaitForSeconds(2f);
 
         Destroy(this.gameObject);
+    }
+
+    bool isPatron()
+    {
+        if (this.gameObject.tag == "Patron")
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
     }
 }
